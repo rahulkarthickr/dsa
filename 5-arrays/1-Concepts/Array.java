fee7fd6a9d762b28e51224de57661318cd6415f9
rahulkarthick <126461935+rahulkarthick@users.noreply.github.com>
2024-09-15 18:02:15 +0530

    update arrays

--- 5-arrays/1-Concepts/Array.java
+++ 5-arrays/1-Concepts/Array.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Array {
@@ -6,10 +7,8 @@ public class Array {
         int[] arr = new int[5];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
-            for (int j = 0; j < arr.length; j++) {
-                System.out.println(arr[i] + " ");
-            }
         }
+        System.out.println(Arrays.toString(arr));
         sc.close();
     }
 }
