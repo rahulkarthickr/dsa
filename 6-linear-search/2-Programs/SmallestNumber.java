82383e3c44582bd6b9451908a87027117cb9a68b
rahulkarthick <126461935+rahulkarthick@users.noreply.github.com>
2024-09-17 22:49:01 +0530

    update linear-search

--- 6-linear-search/2-Programs/SmallestNumber.java
+++ 6-linear-search/2-Programs/SmallestNumber.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Minof {
+public class SmallestNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = { 1, 2, 3, 4 };
