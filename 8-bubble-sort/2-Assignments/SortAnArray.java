990fffe740d6b21f9bed13116e2875f1bee0788c
rahulkarthick <rahulkarthickcce@gmail.com>
2024-10-03 18:48:28 +0530

    update bubble-sort

--- 8-bubble-sort/2-Assignments/SortAnArray.java
+++ 8-bubble-sort/2-Assignments/SortAnArray.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ArraySort {
+public class SortAnArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] nums = new int[sc.nextInt()];
